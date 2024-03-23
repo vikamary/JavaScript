ca6bc2ae7c0d29b0432d986a1fca19b6c2541422
vikamarey <vikamarey@gmail.com>
2024-03-23 22:56:37 +0300

    Second Task

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+//First Task
+
 let x = 20;
 let y = 58;
 let z = 42;
 
 let sum = x + y + z;
 console.log("Результат переменных:", sum);
+
+//Second Task
+
+let secondsInMinute = 60;
+let minutesInHour = 60;
+let hoursInDay = 24;
+let daysInYear = 365;
+
+let myAgeInYears = 21;
+
+let myAgeInSeconds = secondsInMinute * minutesInHour * hoursInDay * daysInYear * myAgeInYears;
+
+console.log("My age in seconds", myAgeInSeconds);
